7981e210e49e8cc64b8217569109588e64e1f687
Margeaux Gebert <margeaux.gebert@gmail.com>
2025-03-07 13:30:37 +0100

    add display single theme

--- src/components/ColorCardSection/ColorCardSection.jsx
+++ src/components/ColorCardSection/ColorCardSection.jsx
@@ -5,8 +5,13 @@ export function ColorCardSection({ title }) {
   return (
     <article className="color-card">
       <h2 className="color-card__title">{title}</h2>
-      {themes[0].colors.map(colors => (
-        <ColorCard value={colors.value} role={colors.role} />
+      {themes[0].colors.map((colors) => (
+        <ColorCard
+          value={colors.value}
+          role={
+            colors.role //.charAt(0).toUpperCase() + colors.role.slice(1)
+          }
+        />
       ))}
     </article>
   );
